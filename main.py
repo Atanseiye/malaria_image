c42a714b3c69497c92acd9a5be022ae08189d05b
Kolade <92696803+Atanseiye@users.noreply.github.com>
2024-08-13 14:48:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,16 +18,14 @@ button = st.button('Predict')
 if button:
 
     # st.write(load_image)
-    try:
-        pic = Image.open(load_image)
-        pic = pic.resize((256, 256))
-        pic_array = np.array(pic)
-        pic_arrays = np.expand_dims(pic_array, 0)
-        pic_array = pic_array.reshape((1, 256, 256, 3))
+    pic = Image.open(load_image)
+    pic = pic.resize((256, 256))
+    pic_array = np.array(pic)
+    pic_arrays = np.expand_dims(pic_array, 0)
+    pic_array = pic_array.reshape((1, 256, 256, 3))
 
 
-        prediction = class_names[np.argmax(model.predict(pic_arrays))]
+    prediction = class_names[np.argmax(model.predict(pic_arrays))]
 
-        st.write(f'The image was predicted to be {prediction}')
-    except:
-        st.write('No image was selected')
+    st.write(f'The image was predicted to be {prediction}')
+ 
