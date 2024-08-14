c17b89eccd0e4a16ab41e9d78989ff8c97020a3b
Kolade <92696803+Atanseiye@users.noreply.github.com>
2024-08-14 11:48:17 +0100

    Update all - main.py

--- main.py
+++ main.py
@@ -5,30 +5,37 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 from io import BytesIO
+import os
 
 st.title('Malaria Detection Model In Image blood smear using Deep Learning ')
 
 class_names = ['Parasite', 'Uninfected']
 load_image = st.file_uploader('Your Image here')
-model = tf.keras.models.load_model('models/1')
+# model = tf.keras.models.load_model('models/1')
+
+
+try:
+    model=tf.keras.models.load_model('models/1')
+    st.write('Model load successfully')
+except:
+    st.write('Issues loading model')
 
 
 button = st.button('Predict')
 
 if button:
 
-    # try:
     # st.write(load_image)
-    st.write(load_image)
-    pic = Image.open(load_image)
-    pic = pic.resize((256, 256))
-    pic_array = np.array(pic)
-    pic_arrays = np.expand_dims(pic_array, 0)
-    pic_array = pic_array.reshape((1, 256, 256, 3))
+    try:
+        pic = Image.open(load_image)
+        pic = pic.resize((256, 256))
+        pic_array = np.array(pic)
+        pic_arrays = np.expand_dims(pic_array, 0)
+        pic_array = pic_array.reshape((1, 256, 256, 3))
 
 
-    prediction = class_names[np.argmax(model.predict(pic_arrays))]
+        prediction = class_names[np.argmax(model.predict(pic_arrays))]
 
-    st.write(f'The image was predicted to be {prediction}')
-     # except:
-     #    wt.write('No image was selected')
+        st.write(f'The image was predicted to be {prediction}')
+    except:
+        st.write('No image was selected')
